4f4bca2a54ae57ca15586bb728016c4c6e7f6824
Kevin Leong <kleong@kvnbaba.local>
2019-08-21 20:58:42 -0500

    added boundary logic and clean up

--- js/box.js
+++ js/box.js
@@ -1,3 +1,4 @@
+// "hero" sprite
 let hero = Sprite({
     x: 500,        // starting x,y position of the sprite
     y: 500,
@@ -8,67 +9,85 @@ let hero = Sprite({
     dy: 10,       // move the sprite 2px to the right every frame
     update: function () {
 
-        let wallTouch = wallCollision();
+        let touchedWallOn = wallCollision(this,wallArray);
 
         if (keyPressed('left')) {
-            if (this.x > 0) {
+            if (this.x > 0 && !touchedWallOn.right) {
                 this.x -= this.dx;
             }
         }
         if (keyPressed('right')) {
-            if (this.x < canvas.width - this.width) {
+            if ((this.x < canvas.width - this.width) && !touchedWallOn.left) {
                 this.x += this.dx;
             }
         }
         if (keyPressed('up')) {
-            if (this.y > 0) {
+            if (this.y > 0 && !touchedWallOn.bottom) {
                 this.y -= this.dy;
             }
         }
         if (keyPressed('down')) {
-            if (this.y < canvas.height - this.height) {
+            if ((this.y < canvas.height - this.height) && !touchedWallOn.top) {
                 this.y += this.dy;
             }
         }
     }
 });
 
-function collidesWithWall(object, wall) {
-    if (this.rotation || object.rotation) return null;
-
-    // take into account sprite anchors
-    let x = this.x - this.width * this.anchor.x;
-    let y = this.y - this.height * this.anchor.y;
-
-    let objX = object.x;
-    let objY = object.y;
-    if (object.anchor) {
-        objX -= object.width * object.anchor.x;
-        objY -= object.height * object.anchor.y;
-    }
-
-    let collideObj = {
-        left: x < objX + object.width,
-        right: x + this.width > objX,
-        up: y < objY + object.height,
-        down: y + this.height > objY,
+//tool to get exact boundaries of a rect sprite
+let getSpriteBoundaries = function(s) {
+    return {
+        top: s.y - s.height * s.anchor.y,
+        left: s.x - s.width * s.anchor.x,
+        bottom: (s.y - s.height * s.anchor.y) + s.height * (1-s.anchor.y),
+        right: (s.x - s.width * s.anchor.x) + s.width * (1-s.anchor.x)
     };
+};
 
-    return collideObj;
-}
+// returns object on the direction a w sprite was touched by s
+let touchWall = function(s,w) {
+    let wb = getSpriteBoundaries(w);
+    let sb = getSpriteBoundaries(s);
+    return {
+        top: (sb.bottom === wb.top && sb.left < wb.right && sb.right > wb.left),
+        bottom: (sb.top === wb.bottom && sb.left < wb.right && sb.right > wb.left),
+        left: (sb.right === wb.left && sb.top < wb.bottom && sb.bottom > wb.top),
+        right: (sb.left === wb.right && sb.top < wb.bottom && sb.bottom > wb.top),
+    };
+};
 
+// checks collision of all walls in wallArray
+let wallCollision = function (sprite,wArray) {
+    var collided = {
+        right: false,
+        left: false,
+        top: false,
+        bottom: false
+    };
+    wArray.forEach(function (wall, index) {
+        touchedWallOn = touchWall(sprite, wall);
+        for (let dirKey in touchedWallOn) {
+            if (collided[dirKey] === false && touchedWallOn[dirKey] === true) {
+                collided[dirKey] = true
+            }
+        };
+    });
+    return collided;
+};
+
+// wrapper for creating a wall sprite
 let getWallSprite = function(x,y,width,height,color='black') {
-    let wall =  Sprite({
+    return Sprite({
         x: x,        
         y: y,
         color: color,  
         width: width,     
         height: height,
-        collidesWith: collidesWithWall
     });
-    return wall;
-}
-let wallThickness = 10
+};
+
+// testing/implementing walls
+let wallThickness = 10;
 let wallArray = [
     //walls of canvas
     getWallSprite(0, 0, canvas.width, wallThickness),
@@ -82,26 +101,9 @@ let wallArray = [
     //tower area
     getWallSprite(100, 100, wallThickness, canvas.height-100),
 
-    //test wall
-    getWallSprite(200, 400, 300, wallThickness),
+    //test walls
+    getWallSprite(200, 350, 300, wallThickness),
+    getWallSprite(350, 200, wallThickness, 300),
 
-]
+];
 
-let wallCollision = function() {
-    var collided = {
-        right: false,
-        left: false,
-        down: false,
-        up: false
-    };
-    wallArray.forEach(function (wall, index) {
-        singleCollision = wall.collidesWith(hero);
-        console.log([index,singleCollision]);
-        for (let dirKey in singleCollision){
-            if (collided[dirKey] === false && singleCollision[dirKey] === true) {
-                collided[dirKey] = true
-            }
-        };
-    });
-    return collided; 
-}
