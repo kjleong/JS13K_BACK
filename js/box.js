5965590e6c78646504dbbf1e637e634431733450
quangthuyhoang <qtnhoang@gmail.com>
2019-08-25 16:19:01 -0500

    update enemy js connect

--- js/box.js
+++ js/box.js
@@ -11,7 +11,7 @@ let hero = Sprite({
     dy: 10,       // move the sprite 2px to the right every frame
     // animations: spriteSheet.animations,
     update: function () {
-
+     
         let touchedWallOn = wallCollision(this,wallArray);
 
         if (keyPressed('left')) {
