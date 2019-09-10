0bacb803d5dc49de159c0432b8963b87191a9e32
quangthuyhoang <qtnhoang@gmail.com>
2019-09-09 22:00:27 -0500

    update design level

--- js/level.js
+++ js/level.js
@@ -1,6 +1,17 @@
 
 
 // Stats
+let heroStartLoc = [
+    {x: 550, y: 120}, // top right
+    {x: 300, y: 300}, // center
+    {x: 120, y: 120}, // top left
+]
+
+function randArray(array){
+    let index = Math.floor(Math.random()*array.length);
+    return array[index];
+}
+
 let writeStat = function (ctx, x,y,label,value) {
     ctx.fillText(label, x, y);
     x += ctx.measureText(label).width;
@@ -31,8 +42,8 @@ let startLevels = function (gameState, pieces) {
     if (!gameState.floorStarted) {
 
         switch (gameState.floor) {
-            case 12: testlevel12(gameState, pieces); break;//testlevel12(gameState, pieces); break;
-            case 11: testlevel11(gameState, pieces); break;
+            case 12: level7(gameState, pieces); break;//testlevel12(gameState, pieces); break;//testlevel12(gameState, pieces); break;
+            case 11: level7(gameState, pieces); break;//testlevel11(gameState, pieces); break;
             case 10: level10(gameState, pieces); break;
             case 9: level9(gameState, pieces); break;
             case 8: level8(gameState, pieces); break;
@@ -72,13 +83,32 @@ let initializeLevel = function (gameState, pieces) {
 }
 
 let sandboxLevel = function (gameState,pieces) {
-
+    gameState.floorStarted = true;
     let hero = initializeLevel(gameState, pieces);
     hero.sprite.x = 120;
     hero.sprite.y = 550;
-}
 
-//Levels
+    new Heart('h1', 400, 150).addToPieces(pieces);
+}
+// Sandbox
+// let testlevel12 = function (gameState,pieces) {
+
+//     let hero = initializeLevel(gameState, pieces);
+//     hero.sprite.x = 300;
+//     hero.sprite.y = 300;
+//     new Heart('h1', 400, 150).addToPieces(pieces);
+//     new Heart('h2', 300, 150).addToPieces(pieces);
+//     new SwordItem('s1',200, 200).addToPieces(pieces);
+    
+//     new Enemy('e_12_1', 'enemy', 300, 500, 20, 20, 'grey', 'horizontal').addToPieces(pieces);
+//     new Enemy('e_12_1', 'enemy', 300, 500, 20, 20, 'grey', 'vertical').addToPieces(pieces);
+
+//     new Stairs('stair1', 300, 520).addToPieces(pieces);
+    
+// }
+
+
+// Levels
 let testlevel12 = function (gameState,pieces) {
 
     let hero = initializeLevel(gameState, pieces);
@@ -97,7 +127,7 @@ let testlevel12 = function (gameState,pieces) {
     new Move('i_blue',  200, 200, 20, 20, 'blue').addToPieces(pieces);
 
     // Enemies
-    var a = new Enemy('e_red', 'enemy', 150, 200, 20, 20, 'red', 'horizontal').addToPieces(pieces);
+    // var a = new Enemy('e_red', 'enemy', 150, 200, 20, 20, 'red', 'horizontal').addToPieces(pieces);
     var b = new Enemy('e_purple', 'enemy', 250, 200, 20, 20, 'purple', 'vertical', 200, 5).addToPieces(pieces);
 
     new Heart('h1', 400, 150).addToPieces(pieces);
@@ -113,7 +143,7 @@ let testlevel11 = function (gameState, pieces) {
     hero.sprite.x = 120;
     hero.sprite.y = 550;
 
-    var a = new Enemy('e1', 'enemy', 150, 200, 20, 20, 'red', 'vertical').addToPieces(pieces);
+     var a = new Enemy('e1', 'enemy', 150, 200, 20, 20, 'red', 'vertical').addToPieces(pieces);
     new Stairs('stair1', 550, 120).addToPieces(pieces);
 
 }
@@ -143,11 +173,62 @@ let level9 = function (gameState,pieces) {
 
 }
 
-let level8 = function (gameState,pieces) { }
+// depression level - need to change hero speed to slow
+let level8 = function (gameState,pieces) { 
+    
+    let hero = initializeLevel(gameState, pieces);
+    hero.sprite.x = 550;
+    hero.sprite.y = 550;
+    new Wall('w7', 'v', 200, 200, 500).addToPieces(pieces);
+    new Heart('h2', 300, 150).addToPieces(pieces);
+    new SwordItem('s1',120, 330).addToPieces(pieces);
+    new Enemy('e_purple0', 'enemy', 120, 200, 20, 20, 'grey', 'vertical', 220, 5).addToPieces(pieces);
+    new Enemy('e_purple1', 'enemy', 120, 400, 20, 20, 'purple', 'vertical', 220, 5).addToPieces(pieces);
+    new Enemy('e_purple2', 'enemy', 160, 200, 20, 20, 'purple', 'vertical', 100, 5).addToPieces(pieces);
+    new Enemy('e_purple3', 'enemy', 200, 300, 20, 20, 'purple', 'vertical', 200, 5).addToPieces(pieces);
+    new Enemy('e_purple4', 'enemy', 240, 200, 20, 20, 'red', 'vertical', 100, 5).addToPieces(pieces);
+    new Enemy('e_purple5', 'enemy', 280, 300, 20, 20, 'purple', 'vertical', 150, 5).addToPieces(pieces);
+    new Enemy('e_purple11', 'enemy', 340, 300, 20, 20, 'purple', 'vertical', 150, 5).addToPieces(pieces);
+    new Enemy('e_purple12', 'enemy', 400, 300, 20, 20, 'red', 'vertical', 185, 5).addToPieces(pieces);
+    new Enemy('e_purple13', 'enemy', 440, 300, 20, 20, 'red', 'vertical', 185, 7).addToPieces(pieces);
+    new Enemy('e_purple16', 'enemy', 550, 300, 20, 20, 'grey', 'vertical', 185, 1).addToPieces(pieces);
 
-let level7 = function (gameState,pieces) { }
+    new Enemy('e_purple10', 'enemy', 400, 400, 20, 20, 'yellow', 'horizontal', 150, 1).addToPieces(pieces);
+    new Enemy('e_purple9', 'enemy', 340, 380, 20, 20, 'yellow', 'horizontal', 150, 6).addToPieces(pieces);
+    new Enemy('e_purple6', 'enemy', 280, 300, 20, 20, 'purple', 'horizontal', 150, 5).addToPieces(pieces);
+    new Enemy('e_purple7', 'enemy', 280, 340, 20, 20, 'purple', 'horizontal', 150, 5).addToPieces(pieces);
+    new Enemy('e_purple8', 'enemy', 280, 120, 20, 20, 'yellow', 'horizontal', 150, 5).addToPieces(pieces);
+    new Enemy('e_purple14', 'enemy', 280, 500, 20, 20, 'yellow', 'horizontal', 150, 5).addToPieces(pieces);
+    new Enemy('e_purple15', 'enemy', 300, 550, 20, 20, 'yellow', 'horizontal', 150, 5).addToPieces(pieces);
+    
+    new Stairs('stair1', 120, 120).addToPieces(pieces);
+    
+}
 
-let level6 = function (gameState,pieces) { }
+// Narrow path
+let level7 = function (gameState,pieces) { 
+    let hero = initializeLevel(gameState, pieces);
+    hero.sprite.x = 550;
+    hero.sprite.y = 550;
+    new Wall('w7', 'h', 200, 200, 500).addToPieces(pieces);
+    new Wall('w9', 'h', 200, 240, 340).addToPieces(pieces);
+    new Wall('w10', 'v', 530, 250, 120).addToPieces(pieces);
+    new Wall('w11', 'h', 530, 370, 60).addToPieces(pieces);
+
+    new Move('b0', 500, 550, 20, 20, 'grey').addToPieces(pieces);
+    new Move('b1', 220, 210, 20, 20, 'grey').addToPieces(pieces);
+    new Move('b2', 240, 220, 20, 20, 'grey').addToPieces(pieces);
+    new Move('b3', 260, 220, 20, 20, 'grey').addToPieces(pieces);
+
+    new Enemy('e_purple15', 'enemy', 460, 550, 20, 20, 'yellow', 'horizontal', 150, 5).addToPieces(pieces);
+    new Enemy('e_purple16', 'enemy', 300, 210, 20, 20, 'yellow', 'horizontal', 150, 1).addToPieces(pieces);
+    new Enemy('e_purple17', 'enemy', 555, 300, 20, 20, 'yellow', 'vertical', 150, 1).addToPieces(pieces);
+    
+    new Stairs('stair1', 550, 330).addToPieces(pieces);
+}
+
+// blocks stuck in wall to hide shortcuts
+let level6 = function (gameState,pieces) { } 
 
 let level5 = function (gameState,pieces) { }
 
